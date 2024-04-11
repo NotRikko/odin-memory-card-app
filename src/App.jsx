67af038ea0ec149246cf37b181f348817e54a9d1
Rikko <bulletbill51@yahoo.com>
2024-04-11 01:29:23 -0700

    Now tracks chosen cards and score
    Implemented useState for chosen cards
    Implemented useState for score

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,26 @@ import './App.css'
 
 function App() {
 
+  const [chosenCards, setChosenCards] = useState([])
 
-  //const cardCount = 6;
+  const [score, setScore] = useState(0);
+
+  const pickCard = (name) => {
+    if(chosenCards.includes(name)) {            
+        setChosenCards([]);
+        setScore(0);
+    } else {
+        setChosenCards(prevChosenCards => [...prevChosenCards, name])
+        setScore(score => score +1)
+    }
+  }
+
+  useEffect(() => {
+
+  })
+
   const createCards = () => {
     const TestDataCopy = [...TestData];
-    console.log(TestDataCopy);
     const cards = [];
     for(let i=0; i<TestData.length; i++) {
       const randomIndex = Math.floor(Math.random() * TestDataCopy.length);
@@ -19,6 +34,7 @@ function App() {
           index={randomIndex}
           name={TestDataCopy[randomIndex].name} 
           pic={TestDataCopy[randomIndex].imgSrc} 
+          pickCard={pickCard}
         />
       
       );
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,13 @@
 import './CardStyle.css'
 
-export default function Card({name, pic}) {
+export default function Card({name, pic, pickCard}) {
+
+    const handleClick = () => {
+        pickCard(name);
+    }
+
     return (
-        <div className="card">
+        <div className="card" onClick={handleClick}>
             <img src={pic}></img>
             <h3>{name}</h3>
         </div>
