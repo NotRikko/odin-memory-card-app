74a113dbf10093a2373988a5c039297f1f41c2ef
Rikko <bulletbill51@yahoo.com>
2024-04-11 13:27:16 -0700

    Finished styling
    Changed flexbox of cards container to grid
    Added img in App.jsx to display background img
    Added borders to cards
    Created div to seperate scores in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -50,17 +50,20 @@ function App() {
 
 
   return (
-    <>
+    <div id="mainContainer">
       <div id="header">
         <h1>Memorization Game</h1>
-        <h3>Score: {score}</h3>
-        <h3>Score: {highScore}</h3>
+        <div id="scoreBox">
+          <h3>Score: {score}</h3>
+          <h3>High Score: {highScore}</h3>
+        </div>
       </div>
       <div id="cards">
         {createCards()}
       </div>
-     
-    </>
+      <img id="bgImg" src="https://rare-gallery.com/livewalls/imgpreview/96605-Takodachi-Holo.jpg"></img>
+
+    </div>
   )
 }
 
